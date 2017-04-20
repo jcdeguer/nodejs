1b50c43217c8b1fb8eef42ec73699b390f804e3b
Juan Carlos <juan.deguer@gmail.com>
2017-04-20 09:49:28 -0300

    Update user_add.pp

--- manifests/user_add.pp
+++ manifests/user_add.pp
@@ -13,21 +13,21 @@ class nodejs::user_add inherits nodejs::params {
     groups  => [$grupo_nodejs,$grupo_rsync],
   }->
   exec { 'crear_bash_profile':
-    command => '/usr/bin/touch /apps-node/.bash_profile && cat /etc/skel/.bash_profile > /apps-node/.bash_profile',
+    command => '/usr/bin/touch /node/.bash_profile && cat /etc/skel/.bash_profile > /node/.bash_profile',
   }->
   exec { 'copia_eskel_bash_profile':
-    command => '/usr/bin/cat /etc/skel/.bash_profile > /apps-node/.bash_profile',
+    command => '/usr/bin/cat /etc/skel/.bash_profile > /node/.bash_profile',
   }->
   exec { 'crea_bashrc':
-    command => '/usr/bin/touch /apps-node/.bashrc',
+    command => '/usr/bin/touch /node/.bashrc',
   }->
   exec { 'copia_skel_bashrc':
-    command => '/usr/bin/cat /etc/skel/.bashrc > /apps-node/.bashrc',
+    command => '/usr/bin/cat /etc/skel/.bashrc > /node/.bashrc',
   }->
   exec { 'modi_priv_node_bashrc':
-    command => '/usr/bin/chmod 700 /apps-node/.bashrc ',
+    command => '/usr/bin/chmod 700 /node/.bashrc ',
   }->
   exec { 'modi_own_node_bashrc':
-    command => '/usr/bin/chown node:node /apps-node/.bashrc ',
+    command => '/usr/bin/chown node:node /node/.bashrc ',
   }
 }
