4ff6d7eae4ef18e68035b79caef1f64e9eee2935
Juan Carlos <juan.deguer@gmail.com>
2017-04-20 09:44:44 -0300

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -63,13 +63,6 @@ class nodejs::config inherits nodejs::params {
         group => 'rsync',
         mode => 0770,
   }
-  file { $init_test_app:
-        ensure => present,
-        owner => 'root',
-        group => 'root',
-        mode => 0755,
-        content => template ('nodejs/ScritDeInicioNode-test.erb'),
-  }
   file { $archivo_sudoers:
         ensure => present
   }
@@ -89,7 +82,7 @@ class nodejs::config inherits nodejs::params {
         match => "export PATH",
   }->
   file_line { 'paths':
-        line => 'PATH=/apps-node/.npm-global/bin:/usr/local/bin:/usr/local/sbin:~/bin/usr/local/share/npm/bin:$PATH',
+        line => 'PATH=/node/.npm-global/bin:/usr/local/bin:/usr/local/sbin:~/bin/usr/local/share/npm/bin:$PATH',
         path => $bash_profile,
   }->
   file_line { 'export_paths':
@@ -98,7 +91,7 @@ class nodejs::config inherits nodejs::params {
         match => "export PATH",
   }->
   file_line { 'node_paths':
-        line => 'NODE_PATH=/apps-node/.npm-global/lib/node_modules/:$NODE_PATH',
+        line => 'NODE_PATH=/node/.npm-global/lib/node_modules/:$NODE_PATH',
         path => $bash_profile,
   }->
   file_line { 'export_node_paths':
@@ -131,15 +124,15 @@ class nodejs::config inherits nodejs::params {
 
 # Instalacion del modulo oracledb
   exec { 'seteo_prefixs_en_npm':
-        command => '/usr/bin/npm config set prefix "/apps-node/.npm-global/"',
+        command => '/usr/bin/npm config set prefix "/node/.npm-global/"',
   }->
   exec { 'seteo_proxi_en_npm':
-        command => '/usr/bin/npm config set proxy="http://IP_PROXY:PUERTO_PROXY"',
+        command => '/usr/bin/npm config set proxy="http://IP_PROXY:PUERTO_PROXY"', # Donde IP_PROXY:PUERTO_PROXY corresponde a tu configuracion proxy
   }
   exec { 'install_oracldb_con_npm': # Este modulo valida si existe oracldb instalado, en caso contrario, lo instala.
         cwd => '/apps-node/',
-        command => '/usr/bin/npm install -g oracledb --prefix /apps-node/.npm-global/ -ve',
+        command => '/usr/bin/npm install -g oracledb --prefix /node/.npm-global/ -ve',
         user => 'node',
-        creates => '/apps-node/.npm-global/lib/node_modules/oracledb',
+        creates => '/node/.npm-global/lib/node_modules/oracledb',
   }
 }
