6796613d5ea1250731ec62b9c7b39c1f123e65da
Juan Carlos <juan.deguer@gmail.com>
2017-10-26 10:13:42 -0300

    Delete group_add.pp

--- manifests/group_add.pp
+++
@@ -1,9 +0,0 @@
-class nodejs::group_add inherits nodejs::params {
-  group { $grupo_rsync:
-        ensure => 'present',
-        gid    => '9999',
-  }
-  group { $grupo_nodejs:
-        ensure => 'present',
-  }
-}
