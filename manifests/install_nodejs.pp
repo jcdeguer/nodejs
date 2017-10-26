df7238d26f022058ff53fe4150d6894e05a36912
Juan Carlos <juan.deguer@gmail.com>
2017-10-26 10:15:39 -0300

    Delete install_nodejs.pp

--- manifests/install_nodejs.pp
+++
@@ -1,8 +0,0 @@
-class nodejs::install_nodejs inherits nodejs::params {
-  package { $version_nodejs:
-        ensure => 'installed'
-  }
-  package { $version_npm:
-        ensure => 'installed'
-  }
-}
